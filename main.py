c46269a7a65b00482c976947e029ec6182c0758b
Inma <Inma@Inma.local>
2019-04-30 18:35:38 +0200

    Añado imagen termometro

--- main.py
+++ main.py
@@ -1,6 +1,11 @@
 import pygame, sys
 from pygame.locals import *
 
+class Termometro():
+    def __init__(self):
+        self.custome = pygame.image.load("images/term.png")
+
+
 class mainApp():
     termometro = None
     entrada = None
@@ -10,6 +15,7 @@ class mainApp():
         self.__screen = pygame.display.set_mode((290, 415)) #tamaño pantalla
         pygame.display.set_caption("Termometro") #nombre pantalla
         self.__screen.fill((244, 236, 203)) #color de la pantalla
+        self.termometro = Termometro()
         
     def __on_close(self):
         pygame.quit()
@@ -21,12 +27,10 @@ class mainApp():
                 if event.type == QUIT:
                     self.__on_close()
                     
+            self.__screen.blit(self.termometro.custome, (30, 150)) #dibuje el termometro   
             pygame.display.flip()
             
-                   
-                    
-        
-        
+                       
     
 if __name__ == "__main__":
     pygame.init()
